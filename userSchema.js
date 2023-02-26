e1ac142b337d47ef4093ecf3ef5ef9f80210932d
Siddhu1925 <114770885+Siddhu1925@users.noreply.github.com>
2023-02-26 23:15:36 +0530

    first commit

--- userSchema.js
+++ userSchema.js
@@ -3,27 +3,32 @@ const Schema =mongoose.Schema
 
 const userSchema=new Schema({
     name:{
-        type:String,
-        require:true
-    },
-    email:{
-        type:String,
-        require:true,
-        unique:true
+        type: String,
+        required :true,
     },
     username:{
-        type:String,
-        require:true,
-        unique:true
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
     },
     phonenumber:{
-     type:Number,
-     require:true
+        type: Number,
+        required: true,
+        minlength:10,
+        maxlength:11
     },
     password:{
-        type:String,
-        require:true
-    }
+        type: String,
+        required: true,
+    },
+    emailAuthenticated: {
+            type: Boolean,
+  },
 })
 
 module.exports=mongoose.model("users",userSchema)
